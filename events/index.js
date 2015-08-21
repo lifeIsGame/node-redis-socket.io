f3fff45e9422029da5089c7a81aa35f385c652fc
mukharamov <rustem.mukharamov@lezgro.com>
2015-08-21 13:01:09 +0300

    final changes

--- events/index.js
+++ events/index.js
@@ -5,30 +5,48 @@ var sub = redis.createClient();
 var pub = redis.createClient();
 sub.subscribe('chat');
 
-module.exports = function(io) {
-    io.on('connection', function(err, socket, session) {
-        if (!session.user) return;
-        socket.on('chat', function(data) {
-            var msg = JSON.parse(data);
-            var reply = JSON.stringify({
-                action: 'message',
-                user: session.user,
-                msg: msg.msg
-            });
-            pub.publish('chat', reply);
-        });
+module.exports = function (io) {
+  io.on('connection', function (err, socket, session) {
+    if (!session.user) return;
+    socket.on('chat', function (data) {
+      var msg = JSON.parse(data);
+      var reply = JSON.stringify({
+        action: 'message',
+        user: session.user,
+        msg: msg.msg
+      });
+      pub.publish('chat', reply);
+    });
 
-        socket.on('join', function() {
-            var reply = JSON.stringify({
-                action: 'control',
-                user: session.user,
-                msg: ' joined the channel'
-            });
-            pub.publish('chat', reply);
-        });
+    socket.on('typing', function () {
+      var reply = JSON.stringify({
+        action: 'typing',
+        user: session.user
+      });
+      pub.publish('chat', reply);
+    });
 
-        sub.on('message', function(channel, message) {
-            socket.emit(channel, message);
-        });
-    })
+    socket.on('join', function () {
+      var reply = JSON.stringify({
+        action: 'control',
+        user: session.user,
+        msg: ' joined the channel'
+      });
+      pub.publish('chat', reply);
+    });
+
+    sub.on('message', function (channel, message) {
+      socket.emit(channel, message);
+    });
+
+    socket.on('disconnect', function() {
+      var reply = JSON.stringify({
+        action: 'control',
+        user: session.user,
+        msg: ' leave the channel'
+      });
+      pub.publish('chat', reply);
+    });
+
+  })
 };
